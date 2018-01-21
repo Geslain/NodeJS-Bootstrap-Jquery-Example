444ae6307b797a073b0b7736dc06c7f4ae8d79fa
Geslain Dahan <geslain.dahan@keyrus.com>
2018-01-21 19:18:08 +0100

    add commentary section

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    $('.carousel').carousel()
+
+ $(".commentary-form").submit(function () {
+     let commentary = $(".commentary-form-textarea textarea").val();
+
+     let htmlCommentary = $("<li class='commentary'>");
+     let htmlCommentaryHeader = $("<div class='commentary-header'>");
+     let htmlCommentaryBody = $("<div class='commentary-body'>");
+
+     htmlCommentaryHeader = htmlCommentaryHeader.append("<img src='/images/anonymous.jpg' width='50px'><span class='pseudo'>Anonymous</span>");
+     htmlCommentaryBody = htmlCommentaryBody.append(commentary);
+
+     htmlCommentary = htmlCommentary.append(htmlCommentaryHeader).append(htmlCommentaryBody);
+
+     $(".commentary-list").append(htmlCommentary);
+     return false;
+ });
+
+ //$(".commentary-form button")
+ //$(".commentary-form button.submit")
+ //$("#submit")
+ //$("button[name='submit']")
+
 });
